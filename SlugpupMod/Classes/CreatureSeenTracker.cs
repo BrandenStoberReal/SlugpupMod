abf81118cd940c5a694f83961bcd30cb52d05936
Branden Stober <brandenstober@outlook.com>
2023-05-17 07:43:57 -0400

    Updated seen tracker entity list

--- SlugpupMod/Classes/CreatureSeenTracker.cs
+++ SlugpupMod/Classes/CreatureSeenTracker.cs
@@ -8,16 +8,24 @@ namespace SlugpupMod.Classes
 {
     public class CreatureSeenTracker
     {
-        Player AssociatedPlayer { get; set; }
+        public Player AssociatedPlayer { get; set; }
 
-        int GreenLizardsSeen{ get; set; } = 0;
-        int BlueLizardsSeen { get; set; } = 0;
-        int PinkLizardsSeen { get; set; } = 0;
-        int WhiteLizardsSeen { get; set; } = 0;
-        int YellowLizardsSeen { get; set; } = 0;
-        int CentipedesSeen { get; set; } = 0;
-        int DropwingsSeen { get; set; } = 0;
-        int VulturesSeen { get; set; } = 0;
+        public int GreenLizardSightings { get; set; } = 0;
+        public int BlueLizardSightings { get; set; } = 0;
+        public int CyanLizardSightings { get; set; } = 0;
+        public int PinkLizardSightings { get; set; } = 0;
+        public int WhiteLizardSightings { get; set; } = 0;
+        public int YellowLizardSightings { get; set; } = 0;
+        public int BlackLizardSightings { get; set; } = 0;
+        public int RedLizardSightings { get; set; } = 0;
+        public int CentipedeSightings { get; set; } = 0;
+        public int DropwingSightings { get; set; } = 0;
+        public int VultureSightings { get; set; } = 0;
+        public int SpitLizardSightings { get; set; } = 0;
+        public int EelLizardSightings { get; set; } = 0;
+        public int ZoopLizardSightings { get; set; } = 0;
+        public int SalemanderSightings { get; set; } = 0;
+        public int ScavSightings { get; set; } = 0;
 
         // Class instantiation
         public CreatureSeenTracker(Player plr)
